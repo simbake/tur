4b1cd88ce8d08fb52ee7f01c0b432ee77ac7aaa8
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-12-13 22:36:58 +0800

    [skip ci]

--- tur-on-device/r-base/build.sh
+++ tur-on-device/r-base/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.r-project.org/
 TERMUX_PKG_DESCRIPTION="A free software environment for statistical computing and graphics"
 TERMUX_PKG_LICENSE="GPL-2.0, GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=4.2.2
+TERMUX_PKG_VERSION=4.3.2
 TERMUX_PKG_SRCURL=https://mirrors.tuna.tsinghua.edu.cn/CRAN/src/base/R-${TERMUX_PKG_VERSION::1}/R-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=0ff62b42ec51afa5713caee7c4fde7a0c45940ba39bef8c5c9487fef0c953df5
+TERMUX_PKG_SHA256=b3f5760ac2eee8026a3f0eefcb25b47723d978038eee8e844762094c860c452a
 TERMUX_PKG_DEPENDS="libandroid-glob, libiconv, libbz2, libcurl, liblzma, pcre2, readline, zlib"
 TERMUX_PKG_BUILD_DEPENDS="binutils, gcc-11, openjdk-17"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
@@ -16,7 +16,11 @@ ac_cv_have_decl_wctrans=yes
 source $TERMUX_SCRIPTDIR/common-files/setup_toolchain_gcc.sh
 
 termux_step_pre_configure() {
-    CFLAGS="${CFLAGS/-Oz/-O0}"
+	if [ "${TERMUX_ON_DEVICE_BUILD}" = false ]; then
+		termux_error_exit "This package doesn't support cross-compiling."
+	fi
+
+	CFLAGS="${CFLAGS/-Oz/-O0}"
 	CXXFLAGS="${CXXFLAGS/-Oz/-O0}"
 	LDFLAGS="${LDFLAGS/-static-openmp/''}"
 	LDFLAGS+=" -landroid-glob"
