f0ad7b884ca83c4f062d5418f5214d8827e078c8
Oscar Simba <simbake2009@gmail.com>
2024-12-30 01:37:07 +0300

    Update build.sh

--- tur/virglrenderer-mesa-zink/build.sh
+++ tur/virglrenderer-mesa-zink/build.sh
@@ -2,31 +2,24 @@ TERMUX_PKG_HOMEPAGE=https://virgil3d.github.io/
 TERMUX_PKG_DESCRIPTION="A virtual 3D GPU for use inside qemu virtual machines"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION=20220627
-TERMUX_PKG_SRCURL=git+https://gitlab.freedesktop.org/virgl/virglrenderer
-TERMUX_PKG_DEPENDS="libdrm, libepoxy, libglvnd, libx11, mesa-zink"
+TERMUX_PKG_VERSION=1.0.1
+TERMUX_PKG_SRCURL=https://gitlab.freedesktop.org/virgl/virglrenderer/-/archive/virglrenderer-${TERMUX_PKG_VERSION[0]}/virglrenderer-virglrenderer-${TERMUX_PKG_VERSION[0]}.tar.gz
+TERMUX_PKG_DEPENDS="angle-android, libdrm, libepoxy, libglvnd, libx11, mesa, vulkan-loader"
+TERMUX_PKG_SHA256=446ab3e265f574ec598e77bdfbf0616ee3c77361f0574bec733ba4bac4df730a
 TERMUX_PKG_BUILD_DEPENDS="xorgproto"
-TERMUX_PKG_CONFLICTS="virglrenderer"
-TERMUX_PKG_REPLACES="virglrenderer"
-TERMUX_PKG_PROVIDES="virglrenderer"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Dplatforms=egl,glx"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Dplatforms=egl,glx -Dvenus=true"
 
 # Ref: https://github.com/ThieuMinh26/Proot-Setup/blob/25edeff7b45feffc4117276ef8245e94f7682766/Zink
-termux_step_get_source() {
-	local TMP_CHECKOUT=$TERMUX_PKG_CACHEDIR/tmp-checkout
-	local TMP_CHECKOUT_VERSION=$TERMUX_PKG_CACHEDIR/tmp-checkout-version
+termux_step_make_install() {
+	sed "s|@TERMUX_PREFIX@|$TERMUX_PREFIX|g" \
+		$TERMUX_PKG_BUILDER_DIR/virgl_test_server.in > \
+		$TERMUX_PREFIX/bin/virgl_test_server
+	chmod +x $TERMUX_PREFIX/bin/virgl_test_server
+}
 
-	if [ ! -f $TMP_CHECKOUT_VERSION ] || [ "$(cat $TMP_CHECKOUT_VERSION)" != "$TERMUX_PKG_VERSION" ]; then
-		rm -rf $TMP_CHECKOUT
-		git clone --shallow-since 2022-06-27 ${TERMUX_PKG_SRCURL:4} $TMP_CHECKOUT
-
-		pushd $TMP_CHECKOUT
-		git checkout -f dd301caf7e05ec9c09634fb7872067542aad89b7~2
-		popd
-
-		echo "$TERMUX_PKG_VERSION" > $TMP_CHECKOUT_VERSION
-	fi
-
-	rm -rf $TERMUX_PKG_SRCDIR
-	cp -Rf $TMP_CHECKOUT $TERMUX_PKG_SRCDIR
+termux_step_install_license() {
+	mkdir -p $TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME
+	cp $TERMUX_PKG_SRCDIR/COPYING $TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME/COPYING-virglrenderer
+	cp $TERMUX_PKG_SRCDIR/libepoxy/COPYING $TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME/COPYING-libepoxy
+	cp $TERMUX_PKG_BUILDER_DIR/COPYING-gl4es $TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME/COPYING-gl4es
 }
