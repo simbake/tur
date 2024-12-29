0e4395d029f2538559bb8a97332861c97b9e15e4
Oscar Simba <simbake2009@gmail.com>
2024-12-30 01:56:12 +0300

    Update build.sh

--- tur/virglrenderer-mesa-zink/build.sh
+++ tur/virglrenderer-mesa-zink/build.sh
@@ -8,7 +8,11 @@ TERMUX_PKG_DEPENDS="angle-android, libdrm, libepoxy, libglvnd, libx11, mesa, vul
 TERMUX_PKG_SHA256=446ab3e265f574ec598e77bdfbf0616ee3c77361f0574bec733ba4bac4df730a
 TERMUX_PKG_BUILD_DEPENDS="xorgproto"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Dplatforms=egl,glx -Dvenus=true"
-
+termux_step_pre_configure() {
+	# error: using an array subscript expression within 'offsetof' is a Clang extension [-Werror,-Wgnu-offsetof-extensions]
+	# list_for_each_entry_safe(struct vrend_linked_shader_program, ent, &shader->programs, sl[shader->sel->type])
+	CPPFLAGS+=" -Wno-error=gnu-offsetof-extensions"
+}
 # Ref: https://github.com/ThieuMinh26/Proot-Setup/blob/25edeff7b45feffc4117276ef8245e94f7682766/Zink
 termux_step_make_install() {
 	sed "s|@TERMUX_PREFIX@|$TERMUX_PREFIX|g" \
